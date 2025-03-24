61986eefbb739aa0deed39742daa5bf8fb9b75c4
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-03-24 07:05:34 +0300

    Proje dosyası ekle.

---
+++ Core/UdemyCarBook.Domain/Entities/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<TagBlog> TagBlogs { get; set; }
+    }
+}
